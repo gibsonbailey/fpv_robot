0b9abc64ed14dbe2f832c10fdefac536c2ff0794
gibsonbailey <baileytrefts@gmail.com>
2024-10-05 19:01:02 -0700

    Create scripts for building and uploading using arduino-cli.

---
+++ build.sh
@@ -0,0 +1,2 @@
+# This is the fqbn for the Adafruit Arduino Nano Every
+arduino-cli compile --log --fqbn arduino:megaavr:nona4809 main
---
+++ upload.sh
@@ -0,0 +1,7 @@
+PORT=/dev/cu.usbserial-02BA53BD
+PORT=/dev/cu.SLAB_USBtoUART
+PORT=/dev/cu.usbmodem2101
+
+# arduino-cli upload --log --fqbn esp32:esp32:featheresp32 --port $PORT combined
+
+arduino-cli upload --log -p $PORT --fqbn arduino:megaavr:nona4809 main

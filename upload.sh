75f6fe5260d6df1bc527b9997dbabc08c526f8b8
gibsonbailey <baileytrefts@gmail.com>
2025-03-02 12:29:55 -0800

    Add helpful error check in upload to arduino script.

--- upload.sh
+++ upload.sh
@@ -5,4 +5,11 @@ PORT=$(arduino-cli board list | grep "Nano Every" | awk '{print $1}')
 
 # arduino-cli upload --log --fqbn esp32:esp32:featheresp32 --port $PORT combined
 
+if [ -z "$PORT" ]; then
+  echo "No port found"
+  exit 1
+fi
+
+echo "Uploading to $PORT"
+
 arduino-cli upload --log -p $PORT --fqbn arduino:megaavr:nona4809 stepper
